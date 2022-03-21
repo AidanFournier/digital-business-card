c541ce1d707930480e9086e0b144e4040d775c8f
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-03-21 22:24:31 +0900

    Styled About section

--- src/components/About.js
+++ src/components/About.js
@@ -2,8 +2,9 @@ import React from "react"
 
 export default function About() {
     return (
-        <div>
-            <h1>This is the About section</h1>
+        <div className="about-interests">
+            <h1>About</h1>
+            <p>Earlier this year, I took the jump from coding as a side hobby, to immersing myself in a part-time coding bootcamp in Tokyo. It was a crazy and exciting 6 months, and it solidified in my mind that this is where I want to take my career.</p>
         </div>
     )
 }
