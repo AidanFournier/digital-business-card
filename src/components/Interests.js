2981bf272c3f437f459014df5015f410738627ce
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-03-21 22:31:30 +0900

    Styled interests section

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -2,8 +2,9 @@ import React from "react"
 
 export default function Interests() {
     return (
-        <div>
-            <h1>This is the Interests section</h1>
+        <div className="about-interests">
+            <h1>Interests</h1>
+            <p>When I'm not in the classroom or on my computer, I enjoy making friends with the street cats in my neighbourhood. I also like video games such as Pokemon and Animal Crossing.</p>
         </div>
     )
 }
